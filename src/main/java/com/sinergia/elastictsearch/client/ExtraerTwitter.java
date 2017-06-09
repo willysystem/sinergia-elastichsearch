42bbf10b320f611668fdb6b8146e12a49880f259
willysystem <willysystems@gmail.com>
2017-06-08 22:05:01 -0400

    work the indexed

--- src/main/java/com/sinergia/elastictsearch/client/ExtraerTwitter.java
+++ src/main/java/com/sinergia/elastictsearch/client/ExtraerTwitter.java
@@ -12,6 +12,7 @@ import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.transport.client.PreBuiltTransportClient;
+import org.jboss.netty.logging.JBossLoggerFactory;
 
 import com.google.gson.Gson;
 
@@ -24,23 +25,42 @@ import twitter4j.TwitterFactory;
 public class ExtraerTwitter {
 	
 	public static void main(String arg[]) throws TwitterException{
+		//1. Extraer de datos de Twitter 		
+//		List<Object> list = searchForTweets();
+//		for (Object object : list) {
+//			System.out.println("object: " + object);
+//		}
 		
+		//2. 
+		
+		//3. 
 		try {
-			//Settings settings = Settings.builder().put("cluster.name", "elasticsearch").build();
+			Settings settings = Settings.builder().put("cluster.name", "my-application").build();
 			@SuppressWarnings("resource")
-			TransportClient client = new PreBuiltTransportClient(Settings.EMPTY).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localost"), 9301));
-			IndexResponse response = client.prepareIndex("megacorp","employee","1").get();
-			String id = response.getId();
-			System.out.println("id: " + id);
+//			TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("192.168.1.3"), 9300));
+			TransportClient client = new PreBuiltTransportClient(settings).addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("localhost"), 9300));
+			IndexResponse response = client.prepareIndex("megacorp","employee","3").setSource().get();
+			
+			
+			System.out.println("response.getId(): " + response.getId());
+			System.out.println("response.getType(): " + response.getType());
+			System.out.println("response.getClass(): " + response.getClass());
+			System.out.println("response.getIndex(): " + response.getIndex());
+			System.out.println("response.getResult(): " + response.getResult());
+			System.out.println("response.getVersion(): " + response.getVersion());
+			System.out.println("response.getShardId(): " + response.getShardId());
+			System.out.println("response.getShardInfo(): " + response.getShardInfo());
+			System.out.println("response.getShardInfo().getSuccessful(): " + response.getShardInfo().getSuccessful());
+			
+			
+			
+			
 			
 		} catch (UnknownHostException e) {
 			e.printStackTrace();
 		}
 		
-//		List<Object> list = searchForTweets();
-//		for (Object object : list) {
-//			System.out.println("object: " + object);
-//		}
+
 		
 	}
 	
